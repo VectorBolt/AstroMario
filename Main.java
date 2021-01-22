91ca9729136d46a15517a182baf13da35a683745
Avneesh Verma <75852179+VectorBolt@users.noreply.github.com>
2021-01-22 11:34:58 -0500

    Make Sound Quieter

---
+++ Main.java
@@ -0,0 +1,35 @@
+/* Imports */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+  // Window Properties
+  static JFrame window;
+  static final int FRAME_WIDTH = 1280;
+  static final int FRAME_HEIGHT = 720;
+  
+  // Level end conditions
+  static Level[] levels = new Level[4];
+  static boolean died;
+
+  // MAIN METHOD
+  public static void main(String[] args) {
+    // Window
+    window = new JFrame("Space Window");
+    window.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    do {
+      levels[0] = new Level1(window);
+      died = levels[0].myMain();
+      
+    } while (died);
+
+    do {
+      levels[1] = new Level2(window);
+      died = levels[1].myMain();
+      
+    } while (died);
+  }
+}
