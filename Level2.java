f2c7fc9ff2ddc71891cd1b665791b11b0734185e
Avneesh Verma <avneeshverma@Avneeshs-MacBook-Air.local>
2021-01-21 20:38:54 -0500

    Recover Level 2

--- Level2.java
+++ Level2.java
@@ -1,74 +1,88 @@
-/* Imports */
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
-public class Level2 extends Level {
-  
-  /* CONSTRUCTOR */
-  public Level2(JFrame gameWindow) {
-    
-    this.window = gameWindow;
-    this.keyListener = new MyKeyListener(this);
-    this.canvas = new GraphicsPanel(this);
-    
-    // Window
-    this.canvas.addKeyListener(this.keyListener);
-    this.window.add(this.canvas);
-    
-    this.player1 = new Player(FRAME_WIDTH/4, FRAME_HEIGHT-GROUND_HEIGHT-63, 10, -30);
-    
-    // Enemy Properties
-    this.goombas = new Goomba[] {
-      new Goomba(3*FRAME_WIDTH/4, FRAME_HEIGHT-GROUND_HEIGHT-32, 100),
-      new Goomba(FRAME_WIDTH/2 + 150, FRAME_HEIGHT - GROUND_HEIGHT - 232, 100)
-    };
+ /* Imports */
+ import javax.swing.*;
+ import java.awt.*;
+ import java.awt.event.*;
 
-    this.crabs = new Crab[] {
-      new Crab(500, FRAME_HEIGHT-GROUND_HEIGHT-32, 100),
-      new Crab(800, FRAME_HEIGHT-GROUND_HEIGHT-32, 100)
-    };
+ public class Level2 extends Level {
 
-    this.bees = new Bee[] {
-      new Bee(900, 400, 100),
-      new Bee(1200, 500, 250)
-    };
+   /* CONSTRUCTOR */
+   public Level2(JFrame gameWindow) {
 
-    this.flyers = new Flyer[] {
-      new Flyer(1000, 200, 100),
-      new Flyer(1600, 300, 250)
-    };
+     this.window = gameWindow;
+     this.keyListener = new MyKeyListener(this);
+     this.canvas = new GraphicsPanel(this);
 
-    this.enemies = new Enemy[][] {goombas, crabs, bees, flyers};
+     // Window
+     this.canvas.addKeyListener(this.keyListener);
+     this.window.add(this.canvas);
 
-    // Platform properties
-    this.platforms = new int[][] {
-        // End checkpoint/flag
-      {7*FRAME_WIDTH - 375, FRAME_HEIGHT - 250, 75, 150},
-        // Other platforms
-      {FRAME_WIDTH/3, FRAME_HEIGHT - GROUND_HEIGHT - 100, 245, 65},
-      {3*FRAME_WIDTH/4, FRAME_HEIGHT - GROUND_HEIGHT - 250, 245, 65},
-      {FRAME_WIDTH, FRAME_HEIGHT - GROUND_HEIGHT - 160, 245, 65}
-    };
-    
-    // Wall Properties
-    this.walls = new int[][] {
-      // Start wall
-      {-500, 0, 550, FRAME_HEIGHT},
-        // End wall
-      {7*FRAME_WIDTH, 0, 1000, FRAME_HEIGHT}
-    };
+     this.player1 = new Player(FRAME_WIDTH/4, FRAME_HEIGHT-GROUND_HEIGHT-63, 10, -30);
 
-    this.spikes = new int[][] {
-      {500, FRAME_HEIGHT - 130, 125, 30},
-      {675, FRAME_HEIGHT - 130, 125, 30},
-      {850, FRAME_HEIGHT - 130, 125, 30},
-      {1025, FRAME_HEIGHT - 130, 125, 30},
-    };
+     // Enemy Properties
+     this.goombas = new Goomba[] {
+       new Goomba(2022, 278, 100)
+     };
 
-    this.water = new Rectangle[] {
-      new Rectangle(1100, 100, 320, 192),
-      new Rectangle(1420, 100, 320, 192)
-    };
-  }
-}
+     this.crabs = new Crab[] {
+       new Crab(1732, 43, 100),
+       new Crab(1832, 478, 100)
+     };
+
+     this.bees = new Bee[] {
+       new Bee(850, 450, 100)
+     };
+
+     this.flyers = new Flyer[] {
+       new Flyer(1100, 500, 100),
+       new Flyer(1800, 310, 75)
+     };
+
+     this.enemies = new Enemy[][] {goombas, crabs, bees, flyers};
+
+     // Platform properties
+     this.platforms = new int[][] {
+         // End checkpoint/flag
+       {7*FRAME_WIDTH - 375, FRAME_HEIGHT - 250, 75, 150},
+         // Other platforms
+       {1630, 75, 245, 65},
+       {1915, 310, 245, 65},
+       {1730, 510, 245, 65},
+     };
+
+     // Wall Properties
+     this.walls = new int[][] {
+       // Start wall
+       {-500, 0, 550, FRAME_HEIGHT},
+         // End wall
+       {7*FRAME_WIDTH, 0, 1000, FRAME_HEIGHT},
+       // Other Walls
+       {1620, 140, 100, 480},
+       {2160, 0, 100, 480}
+     };
+
+     this.spikes = new int[][] {
+       {500, FRAME_HEIGHT - 130, 125, 30},
+       {700, FRAME_HEIGHT - 130, 125, 30},
+       {900, FRAME_HEIGHT - 130, 125, 30},
+       {1100, FRAME_HEIGHT - 130, 125, 30},
+     };
+
+     this.water = new Rectangle[] {
+       new Rectangle(1400, 428, 320, 192),
+       new Rectangle(1620, 428, 320, 192),
+       new Rectangle(1940, 428, 320, 192),
+       new Rectangle(1400, 236, 320, 192),
+       new Rectangle(1620, 236, 320, 192),
+       new Rectangle(1940, 236, 320, 192),
+       new Rectangle(1400, 44, 320, 192),
+       new Rectangle(1620, 44, 320, 192),
+       new Rectangle(1940, 44, 320, 192),
+     };
+
+
+   }
+
+
+ }
+
