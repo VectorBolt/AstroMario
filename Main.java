ade27f0b4787de0139e17226947b8d8dedb2dc4a
Turtlecuber <55955411+Turtlecuber@users.noreply.github.com>
2021-01-23 16:18:04 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -21,6 +21,7 @@ public class Main {
   static MyKeyListener keyListener = new MyKeyListener();  
   
   // Level end conditions and menu
+  static boolean gameOpen = true;
   static Level[] levels = new Level[4];
   static boolean died;
   static boolean inMenu = true;
@@ -40,48 +41,50 @@ public class Main {
     window.setVisible(true);
     
     // Main menu
-    do {
-      window.repaint();
-      try {
-        Thread.sleep(100);
-      }
-      catch (Exception e) {}
-    } while (inMenu);
-    // Exit menu and load game
-    canvas.removeKeyListener(keyListener);
-    window.removeKeyListener(keyListener);
-    window.remove(canvas);
-    
-    // Level 1
-    do {
-      levels[0] = new Level1(window);
-      died = levels[0].myMain();
-    } while (died);
-
-    // Level 2
-    do {
-      levels[1] = new Level2(window);
-      died = levels[1].myMain();    
-    } while (died);
-    
-    // Level 3
-    do {
-      levels[2] = new Level3(window);
-      died = levels[2].myMain();
-    } while (died);
-    victoryMenu = true;
-    
-    // Victory menu
-    window.add(canvas);
-    canvas.addKeyListener(keyListener);
-    canvas.requestFocusInWindow();
-    do {
-      window.repaint();
-      try {
-        Thread.sleep(100);
-      }
-      catch (Exception e) {}
-    } while (victoryMenu);
+    while (gameOpen) {
+      do {
+        window.repaint();
+        try {
+          Thread.sleep(100);
+        }
+        catch (Exception e) {}
+      } while (inMenu);
+      // Exit menu and load game
+      canvas.removeKeyListener(keyListener);
+      window.removeKeyListener(keyListener);
+      window.remove(canvas);
+      
+      // Level 1
+      do {
+        levels[0] = new Level1(window);
+        died = levels[0].myMain();
+      } while (died);
+      
+      // Level 2
+      do {
+        levels[1] = new Level2(window);
+        died = levels[1].myMain();    
+      } while (died);
+      
+      // Level 3
+      do {
+        levels[2] = new Level3(window);
+        died = levels[2].myMain();
+      } while (died);
+      victoryMenu = true;
+      
+      // Victory menu
+      window.add(canvas);
+      canvas.addKeyListener(keyListener);
+      canvas.requestFocusInWindow();
+      do {
+        window.repaint();
+        try {
+          Thread.sleep(100);
+        }
+        catch (Exception e) {}
+      } while (victoryMenu);
+    }
   }  // main method end
   
   /* GRAPHICS PANEL */
@@ -108,19 +111,19 @@ public class Main {
         // Write extra info
         g.setColor(lightBlue);
         g.setFont(new Font("TimesRoman", Font.ITALIC, 75));
-        g.drawString("Creators:", FRAME_WIDTH/2 - 155, FRAME_HEIGHT/2 + 160);
+        g.drawString("Creators:", FRAME_WIDTH/2 - 155, FRAME_HEIGHT/2 + 165);
         g.setFont(new Font("Helvetica", Font.BOLD, 30));
-        g.drawString("Press Enter to go back to the menu", FRAME_WIDTH/2 - 265, 60);
+        g.drawString("Press ENTER to go back to the menu", FRAME_WIDTH/2 - 275, 60);
         // Game controls
         g.setColor(darkYellow);
-        g.setFont(new Font("Courier", Font.BOLD, 40));
+        g.setFont(new Font("Courier", Font.BOLD, 37));
         g.drawString("Avneesh Verma", FRAME_WIDTH/2 - 465, FRAME_HEIGHT/2 + 250);
         g.drawString("Jeffrey Xu", FRAME_WIDTH/2 + 210, FRAME_HEIGHT/2 + 250);
-        g.drawString("Press W/up to jump", FRAME_WIDTH/2 - 450, FRAME_HEIGHT/2 - 175);
-        g.drawString("Press A/left and D/right to move left and right", FRAME_WIDTH/2 - 450, FRAME_HEIGHT/2 - 125);
-        g.drawString("Press SPACE to shoot", FRAME_WIDTH/2 - 450, FRAME_HEIGHT/2 - 75);
-        g.drawString("Press K to reload", FRAME_WIDTH/2 - 450, FRAME_HEIGHT/2 - 25);
-        g.drawString("Press 1, 2, and 3 to switch guns", FRAME_WIDTH/2 - 450, FRAME_HEIGHT/2 + 25);
+        g.drawString("Press W/up to jump", FRAME_WIDTH/2 - 530, FRAME_HEIGHT/2 - 175);
+        g.drawString("Press A/left and D/right to move left and right", FRAME_WIDTH/2 - 530, FRAME_HEIGHT/2 - 125);
+        g.drawString("Press SPACE to shoot", FRAME_WIDTH/2 - 530, FRAME_HEIGHT/2 - 75);
+        g.drawString("Press K to reload", FRAME_WIDTH/2 - 530, FRAME_HEIGHT/2 - 25);
+        g.drawString("Press 1, 2, and 3 to switch guns", FRAME_WIDTH/2 - 530, FRAME_HEIGHT/2 + 25);
       }
       else if(victoryMenu) {
         g.setColor(lightRed);
@@ -129,7 +132,7 @@ public class Main {
         g.setFont(new Font("TimesRoman", Font.ITALIC, 100));
         g.drawString("You beat the game!", FRAME_WIDTH/2 - 395, 270);
         g.setFont(new Font("Helvetica", Font.BOLD, 30));
-        g.drawString("Press Enter to return to the menu", FRAME_WIDTH/2 - 250, 600);
+        g.drawString("Press SPACE to return to the menu", FRAME_WIDTH/2 - 260, 600);
       }
       else {
         // Draw play button
@@ -191,6 +194,14 @@ public class Main {
             buttonSelected[0] = false;
             buttonSelected[1] = true;
           }
+        }
+      }
+      else if (victoryMenu) {
+        if (key == KeyEvent.VK_SPACE) {
+          victoryMenu = false;
+          inMenu = true;
+          buttonSelected[0] = false;
+          buttonSelected[1] = false;
         }
       }
     }
